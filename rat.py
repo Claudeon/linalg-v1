326b5d24a29f0510196ac83377358e55145f44da
Claudeon R Susanto <55183017+Claudeon@users.noreply.github.com>
2021-11-22 23:12:14 +0800

    Update rat.py

--- rat.py
+++ rat.py
@@ -1,38 +1,74 @@
 from math import *
 
-class Rat:
-    def __init__(self, num, *den):
-        if den:
-            den = den[0]
-            if den > 0:
-                d = gcd(num, den)
+class Rat(object):
+    def __init__(self, arg, *args):
+        if len(args) == 0:
+            if type(arg) == str and len(args) == 0:
+                tup = tuple(arg.split('/'))
+                if len(tup) == 2:
+                    temp = Rat(int(tup[0]), int(tup[1]))
+                    self.num, self.den = temp.num, temp.den
+                elif len(tup) == 1:
+                    self.num, self.den = int(tup[0]), 1
+            elif type(arg) == int and len(args) == 0:
+                self.num, self.den = arg, 1
+        elif len(args) == 1:
+            temp_num, temp_den = int(arg), int(args[0])
+            d = math.gcd(temp_num, temp_den)
+            if d:
+                self.num, self.den = temp_num//d, temp_den//d
+                if self.den < 0:
+                    self.num, self.den = -temp_num//d, -temp_den//d
             else:
-                d = -gcd(num, den)
-            self.num = num//d
-            self.den = den//d
+                self.num, self.den = 0, 1
         else:
-            self.num = num
-            self.den = 1
-            
+            raise TypeError("More than 2 arguments")
+
     def __str__(self):
+        if self.den == 1 or self.num == 0:
+            return str(self.num)
         return f'{self.num}/{self.den}'
-        
+    def __repr__(self):
+        return str(self)
+
+    def __eq__(self, other):
+        if isinstance(other, Rat):
+            if self.num == 0 and other.num == 0:
+                return True
+            return (self.num == other.num) and (self.den == other.den)
+        elif isinstance(other, int):
+            return self == Rat(other)
+        elif isinstance(other, float):
+            return self.num/self.den == other
+        else:
+            raise TypeError("other of unknown type")
+
     def __add__(self, other):
-        if type(other) == int:
-            return Rat(self.num + other*self.den, self.den)
-        return Rat(self.num*other.den + self.den*other.num, self.den*other.den)
-    
-    def __sub__(self, other):
-        if type(other) == int:
-            return Rat(self.num - other*self.den, self.den)
-        return Rat(self.num*other.den - self.den*other.num, self.den*other.den)
-        
+        if isinstance(other, Rat):
+            return Rat(self.num*other.den + self.den*other.num, self.den*other.den)
+        elif isinstance(other, int):
+            return Rat(self.num + self.den*other, self.den)
+        elif isinstance(other, float):
+            return self.num/self.den + other
+        else:
+            raise TypeError("other of unknown type")
     def __mul__(self, other):
-        if type(other) == int:
+        if isinstance(other, Rat):
+            return Rat(self.num*other.num, self.den*other.den)
+        elif isinstance(other, int):
             return Rat(self.num*other, self.den)
-        return Rat(self.num*other.num, self.den*other.den)
-        
-    def __div__(self, other):
-        if type(other) == int:
+        elif isinstance(other, float):
+            return self.num/self.den * other
+        else:
+            raise TypeError("other of unknown type")
+    def __sub__(self, other):
+        return self + (other * (-1))
+    def __truediv__(self, other):
+        if isinstance(other, Rat):
+            return Rat(self.num*other.den, self.den*other.num)
+        elif isinstance(other, int):
             return Rat(self.num, self.den*other)
-        return Rat(self.num*other.den, self.den*other.num)
+        elif isinstance(other, float):
+            return (self.num/self.den) / other
+        else:
+            raise TypeError("other of unknown type")
