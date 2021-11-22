c5ea104c4f0b7726211b11e7a52af77bcd2e8329
Claudeon R Susanto <55183017+Claudeon@users.noreply.github.com>
2021-11-22 23:17:01 +0800

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -1,78 +1,5 @@
-import math
+from rational import *
 
-class Rat(object):
-    def __init__(self, arg, *args):
-        if len(args) == 0:
-            if type(arg) == str and len(args) == 0:
-                tup = tuple(arg.split('/'))
-                if len(tup) == 2:
-                    temp = Rat(int(tup[0]), int(tup[1]))
-                    self.num, self.den = temp.num, temp.den
-                elif len(tup) == 1:
-                    self.num, self.den = int(tup[0]), 1
-            elif type(arg) == int and len(args) == 0:
-                self.num, self.den = arg, 1
-        elif len(args) == 1:
-            temp_num, temp_den = int(arg), int(args[0])
-            d = math.gcd(temp_num, temp_den)
-            if d:
-                self.num, self.den = temp_num//d, temp_den//d
-                if self.den < 0:
-                    self.num, self.den = -temp_num//d, -temp_den//d
-            else:
-                self.num, self.den = 0, 1
-        else:
-            raise TypeError("More than 2 arguments")
-
-    def __str__(self):
-        if self.den == 1 or self.num == 0:
-            return str(self.num)
-        return f'{self.num}/{self.den}'
-    def __repr__(self):
-        return str(self)
-
-    def __eq__(self, other):
-        if isinstance(other, Rat):
-            if self.num == 0 and other.num == 0:
-                return True
-            return (self.num == other.num) and (self.den == other.den)
-        elif isinstance(other, int):
-            return self == Rat(other)
-        elif isinstance(other, float):
-            return self.num/self.den == other
-        else:
-            raise TypeError("other of unknown type")
-
-    def __add__(self, other):
-        if isinstance(other, Rat):
-            return Rat(self.num*other.den + self.den*other.num, self.den*other.den)
-        elif isinstance(other, int):
-            return Rat(self.num + self.den*other, self.den)
-        elif isinstance(other, float):
-            return self.num/self.den + other
-        else:
-            raise TypeError("other of unknown type")
-    def __mul__(self, other):
-        if isinstance(other, Rat):
-            return Rat(self.num*other.num, self.den*other.den)
-        elif isinstance(other, int):
-            return Rat(self.num*other, self.den)
-        elif isinstance(other, float):
-            return self.num/self.den * other
-        else:
-            raise TypeError("other of unknown type")
-    def __sub__(self, other):
-        return self + (other * (-1))
-    def __truediv__(self, other):
-        if isinstance(other, Rat):
-            return Rat(self.num*other.den, self.den*other.num)
-        elif isinstance(other, int):
-            return Rat(self.num, self.den*other)
-        elif isinstance(other, float):
-            return (self.num/self.den) / other
-        else:
-            raise TypeError("other of unknown type")
-            
 #### GLOBAL VARIABLES ####
 INPUT_TXT   = 2
 SPACING     = 9
@@ -176,7 +103,7 @@ class RatMatrix(RealMatrix):
         temp = self.copy()
         for r in range(self.rows):
             for c in range(self.cols):
-                temp[r][c] += + other[r][c]
+                temp[r][c] += other[r][c]
         return temp
     def __mul__(self, other):
         if isinstance(other, RealMatrix):
@@ -249,7 +176,7 @@ class RatMatrix(RealMatrix):
         for c in range(cols):
             r = p
             for r in range(p, rows+1):
-                if r == rows or temp[c][r] != ZERO:
+                if r == rows or temp[r][c] != ZERO:
                     r = r
                     break
             if r == rows:
@@ -300,6 +227,10 @@ class RatMatrix(RealMatrix):
         temp = self.combine(eye(size), vert = False)
         temp = temp.rref().slice(size, 2 * size)
         return temp
+    def det(self):
+        pass
+    def rank(self):
+        pass
 
 def zeros(m,n):
     return RatMatrix(m,n)
@@ -320,7 +251,10 @@ def rref(matrix):
     return matrix.rref()
 def inv(matrix):
     return matrix.inv()
-
+def det(matrix):
+    return matrix.det()
+def rank(matrix):
+    return matrix.rank()
 
 
 class Matrix(RatMatrix):
